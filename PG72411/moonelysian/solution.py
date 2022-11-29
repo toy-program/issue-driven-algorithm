c0917d4fb95e521808ca540a2a993a7e2fe6d532
moonelysian <dandyblack@naver.com>
2022-11-29 13:25:10 +0900

    fix: 프로그래머스 - 메뉴리뉴얼

--- PG72411/moonelysian/solution.py
+++ PG72411/moonelysian/solution.py
@@ -3,13 +3,19 @@ from collections import Counter
 
 def solution(orders, course):
     answer = []
+    
     for c in course:
-      temp = []
+        temp = []
+        
+        for order in orders:
+            combi = com(sorted(order), c)
+            temp += combi
+            
+        counter = Counter(temp)
+            
+        if len(counter) != 0 and max(counter.values()) > 1:
+            for key, value in counter.items():
+                if value == max(counter.values()):
+                    answer.append(''.join(i for i in key))
 
-      for order in orders:
-        combi = com(sorted(order), c)
-        temp += combi
-
-        count = Counter(temp)
-        print
-    return answer
+    return sorted(answer)
